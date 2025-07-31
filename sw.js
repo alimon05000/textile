58638ead6e081e0681b86f5b483b5e1b8d9707f4
alimon05000 <alimsrifrv23@gmail.com>
2025-07-31 13:28:08 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,22 @@
-const CACHE_NAME = 'reminders-v1';
+const CACHE_NAME = 'wisdom-v1';
 const ASSETS = [
     '/',
     '/index.html',
     '/styles.css',
-    '/script.js'
+    '/script.js',
+    'https://fonts.googleapis.com/css2?family=El+Messiri:wght@400;700&display=swap'
 ];
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(ASSETS))
+            .then(cache => cache.addAll(ASSETS))
     );
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-            .then((response) => response || fetch(event.request))
+            .then(response => response || fetch(event.request))
     );
 });
